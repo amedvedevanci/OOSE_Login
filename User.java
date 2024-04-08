d8bdb21638632178c50222afa4304c7d3bcc0130
amedvedevanci <116461631+amedvedevanci@users.noreply.github.com>
2024-04-08 21:30:54 +0100

    Update User.java

--- User.java
+++ User.java
@@ -44,19 +44,18 @@ public class User {
             }
         }
 
-    public String getUsername(String email){
-        HashMap<String,String> users= getUsersHashMap();
-        if(users.containsKey(0+email) || users.containsKey(1+email) || users.containsKey(2+email) || users.containsKey(3+email)){
-            return username;
-        }
-        else{
-            authenticationMessage = "Error fetching username";
-            return authenticationMessage;
-        }
-    }
+    //create account method
+    /*This method takes in email String, password String, and userType int (0: Customer, 1: Secretary, 2: Mechanic, 3: GarageManager) 
+    and creates an account.
 
+    The username is created through concatenating userType and email (eg. 0customer@email.com).
+    Therefore it is vital that any time this method is called, the correct userType is passed in.
 
-    //create account
+    At the moment, null values are addressed by checking for 0 String length (to avoid storing any inputs that have an empty string).
+
+    The method then returns String registrationMessage
+    */
+    
     public String createAccount(String email, String password, int userType){
        HashMap<String,String> users= getUsersHashMap();
 
@@ -80,11 +79,13 @@ public class User {
             registrationMessage = "Account already exists";
         }
 
+        //check if a String value exists for email and password. If either is null, requests to enter values for email and password
         else if(email.length() == 0 || password.length() == 0){
             registrationMessage = "Please enter values for email and password";
         }
 
         //otherwise register user and update the serialised Hashmap
+        //TODO: remove the hashmap updated message
         else{
             users.put(username,password);
             registrationMessage = "Registration successful";
@@ -92,7 +93,7 @@ public class User {
                 outputStream.writeObject(users);
                 System.out.println("HashMap updated and saved successfully!");
 
-                //testing purposes only, delete this
+                // prints the HashMap again for testing purposes only, delete this
                 Set set2 = users.entrySet();
                 Iterator iterator2 = set2.iterator();
                 while(iterator2.hasNext()) {
@@ -102,6 +103,7 @@ public class User {
                 }
 
             } 
+            //this catch basically only exists because I got an error message saying it had to. Better error handling needed.
             catch (IOException e) {
                 e.printStackTrace();
             }
@@ -111,9 +113,19 @@ public class User {
                 
     }
 
-    //login
+    /*login method
+    This method takes in email String, password String and userType int to log an existing user into the system.
+
+    Null values in either email or password input are addressed again using .length() method.
+    Also checking here if a user is trying to log in from the wrong app (basically the app methods pass in the userType according to the 
+    relevant roles, hard-coded which is not ideal)
+    Then password is checked against what is stored in the Hashmap.
+
+    The method then returns String authenticationMessage.
+    */
     public String login(String email, String password, int userType){
         HashMap<String,String> users= getUsersHashMap();
+        
         //print HashMap for testing purposes Only. Remove this block of code
         System.out.println("Deserialized HashMap..");
             // Display content using Iterator
@@ -126,15 +138,17 @@ public class User {
             }
 
         username = userType + email;
-        //return no account found if email does not exist in the system
+        //if length is 0 ie String is empty, update authenticationMessage
         if(email.length() == 0 || password.length() == 0){
             authenticationMessage = "Please enter your credentials";
         }
-        
+
+        //if username is not found in the HashMap, but is found when the email is concatenated with a different int, ask user to login from correct app
         else if(!users.containsKey(username) && (users.containsKey(0+email) || users.containsKey((1+email)) || users.containsKey((2+email)) || users.containsKey((3+email)))){
             authenticationMessage = "Please use correct app to login";
         }
-        
+
+        //if username is not found at all, update authenticationMessage
         else if(!users.containsKey(username)){
             authenticationMessage = "No account found";
         }
