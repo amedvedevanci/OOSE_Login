0fb6f5deb4ce8ce3601d8d8ac1db8cbc1be4312e
amedvedevanci <116461631+amedvedevanci@users.noreply.github.com>
2024-04-10 11:21:26 +0100

    Update SecretaryApp.java

--- SecretaryApp.java
+++ SecretaryApp.java
@@ -15,28 +15,38 @@ public class SecretaryApp {
         String registrationMessage;
         String authenticationMessage;
 
+        int menuSelect;
+
         //instantiate User class
 
         User user = new User();
 
-        String [] signUpLoginOptions = {"Register","Login"};
+        String [] signUpLoginOptions = {"Login"};
         signUpLoginSelect = JOptionPane.showOptionDialog(null,"Please select","Register or Login",0,2,null,signUpLoginOptions,signUpLoginOptions[0]);
         email = JOptionPane.showInputDialog("Enter your email address");
         password = JOptionPane.showInputDialog("Enter your password");
 
         if(signUpLoginSelect==0){
-            registrationMessage = user.createAccount(email,password,userType);
-            System.out.println(registrationMessage);
-        }
-        else if(signUpLoginSelect==1){
             authenticationMessage = user.login(email,password,userType);
             System.out.println(authenticationMessage);
         }
+        
         else{
             authenticationMessage = "Operation cancelled";
             System.out.println(authenticationMessage);
         }
 
         //menuSelect to follow later
+
+        String [] menuOptions = {"Create Customer Account"};
+        menuSelect = JOptionPane.showOptionDialog(null, "Please sekect a menu option", "Menu", 0, 1, null, menuOptions, menuOptions[0]);
+
+        if(menuSelect == 0){
+            userType = 0;
+            email = JOptionPane.showInputDialog("Enter email address");
+            password = JOptionPane.showInputDialog("Enter password");
+            registrationMessage = user.createAccount(email, password, userType);
+            System.out.println(registrationMessage);
+        }
     }
 }
