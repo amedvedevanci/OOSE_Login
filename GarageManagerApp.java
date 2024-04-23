25b891fceea2b1d17377d08d54916bea9b257d99
amedvedevanci <116461631+amedvedevanci@users.noreply.github.com>
2024-04-23 11:56:51 +0100

    fix comparepassword

--- GarageManagerApp.java
+++ GarageManagerApp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
@@ -59,13 +61,32 @@ public class GarageManagerApp {
                         operationCancelled = true;
                     }
                     else{
-                        password = new String(passwordField.getPassword());
-                        user.setPassword(password);
                         if(signUpLoginSelect==0){
-                            registrationMessage = user.createAccount(userType);
-                            System.out.println(registrationMessage);
+                            JPasswordField confirmPasswordField = new JPasswordField();
+                            int confirmPasswordEntry = JOptionPane.showConfirmDialog(null, confirmPasswordField, "Please re-enter password",JOptionPane.OK_CANCEL_OPTION);
+                            
+                            if(confirmPasswordEntry<0||confirmPasswordEntry==JOptionPane.CANCEL_OPTION){
+                                registrationMessage = "Cancelled";
+                                operationCancelled = true;
+                            }
+                            else{
+                                boolean confirmPasswordCheck = Arrays.equals(passwordField.getPassword(), confirmPasswordField.getPassword());
+                                if(confirmPasswordCheck){
+                                    password = new String(passwordField.getPassword());
+                                    user.setPassword(password);
+                                    registrationMessage = user.createAccount(userType);
+                                    System.out.println(registrationMessage);
+                                }
+                                else{
+                                    registrationMessage="Passwords do not match";
+                                    System.out.println(registrationMessage);
+                                    user.setRegistrationSuccessCheck(false);
+                                }
+                            }
                         }
                         else if(signUpLoginSelect==1){
+                            password = new String(passwordField.getPassword());
+                            user.setPassword(password);
                             authenticationMessage = user.login(userType);
                             System.out.println(authenticationMessage);
                         }
