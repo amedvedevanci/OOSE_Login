079dc30300a301b5924fdc1cea4c07fa10ec4cfc
amedvedevanci <116461631+amedvedevanci@users.noreply.github.com>
2024-04-27 15:18:53 +0100

    update user test

--- UserTest.java
+++ UserTest.java
@@ -1,21 +1,51 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 public class UserTest {
+    
+    @BeforeAll
+    static void setup(){
+        User user = new User();
+        user.setEmail("garagemanager@mechanic.com");
+        user.setPassword("P@ssw0rd");
+        user.createAccount(3);
+    }
+
 //unit test for account not found    
-@Test
-public void noAccountFound() {
-    // given
-    User user = new User();
-    user.setEmail("heber.testingstuff@test.com"); 
-    user.setPassword("test123"); 
-    var input = 0;
+    @Test
+    @DisplayName("No account found")
+    public void noAccountFound() {
+        // given
+        User user = new User();
+        user.setEmail("heber.testingstuff@test.com"); 
+        user.setPassword("Test123!"); 
+        user.login(0);
 
-    // when
-    var result = user.login(input);
+        // when
+        boolean result = user.getLoginSuccessCheck();
 
-    // then
-    assertEquals("No account found", result);
-}
+        // then
+        assertEquals(false, result);
+    }
+
+    @Test
+    @DisplayName("Successful Login")
+    public void successfulLogin() {
+        // given
+        User user = new User();
+        user.setEmail("garagemanager@mechanic.com"); 
+        user.setPassword("P@ssw0rd");
+        user.login(3);
+    
+        // when
+        boolean result = user.getLoginSuccessCheck();
+        
+        // then
+        assertEquals(true, result);
+    }
 }
